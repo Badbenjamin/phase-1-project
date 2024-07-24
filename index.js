889f37678feb109ebe6d97a95fd80a1311592487
ben mccarthy <bmccarthy90@gmail.com>
2024-07-24 13:42:34 -0400

    got site functioning with image and info from db.json

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+const burgersList = document.getElementById('burgers-list')
+console.log(burgersList)
+
 fetch("http://localhost:3000/burgers")
 .then(response => response.json())
-.then(data => console.log(data))
+.then(burgers => {
+    burgers.forEach(addBurgerToBurgersList)
+})
 
-// adfjlajdlkfjadf
+function addBurgerToBurgersList(burger) {
+    const liElement = document.createElement('li')
+    const nameElement = document.createElement('h3')
+    nameElement.textContent = `name ${burger.name}`;
+
+    const imageElement = document.createElement('img')
+    imageElement.src = burger.image
+
+    const priceElement = document.createElement('h3');
+    priceElement.textContent = burgersList.price;
+
+    liElement.appendChild(nameElement)
+    burgersList.appendChild(liElement)
+    liElement.appendChild(imageElement)
+}
